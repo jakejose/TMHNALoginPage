40afc4f5444bff4bc998b7de3b04d18be52f4609
Jacob Joseph <jakejose@iu.edu>
2021-12-08 18:42:41 -0500

    working webpage

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function toggleResetPswd(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle() // display:block or none
+    $('#logreg-forms .form-reset').toggle() // display:block or none
+}
+
+function toggleSignUp(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle(); // display:block or none
+    $('#logreg-forms .form-signup').toggle(); // display:block or none
+}
+
+
+
+$(()=>{
+    // Login Register Form
+    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+    $('#logreg-forms #btn-signup').click(toggleSignUp);
+    $('#logreg-forms #cancel_signup').click(toggleSignUp);
+    // https://developer.mozilla.org/en-US/docs/Web/API/Location/reload
+    $('#logreg-forms .form-signin').submit(function(e){
+        e.preventDefault();
+        window.location.replace('https://developer.mozilla.org/en-US/docs/Web/API/Location.reload');
+    });
+    $('#logreg-forms .form-signup').submit(function(e){
+        e.preventDefault();
+        confirm("Your Request has been sent!");
+        window.location.replace('index.html');
+    });
+})
